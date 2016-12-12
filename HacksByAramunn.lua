4d51834596db69304d4a59b6d69770769bfffff5
aramunn <aramunn@users.noreply.github.com>
2016-12-11 23:19:12 -0500

    add settings display

--- HacksByAramunn.lua
+++ HacksByAramunn.lua
@@ -2,26 +2,29 @@ local HacksByAramunn = {}
 
 function HacksByAramunn:GenHacks()
   self.tHacks = {
-    
-    FrameCountDeprecation = {
-      Init = function()
-        Apollo.RegisterEventHandler("FrameCount", "OnFrameCount", self.tHacks.FrameCountDeprecation)
+    {
+      Name = "Frame Count Deprecation",
+      Description = "this is a long description blah blah blah blah blah blah blah blah blah blah blah blah",
+      Load = function(ref)
+        Apollo.RegisterEventHandler("FrameCount", "OnFrameCount", ref)
       end,
-      OnFrameCount = function()
-        Event_FireGenericEvent("VarChange_FrameCount")
+      Unload = function(ref)
+        Apollo.RemoveEventHandler("FrameCount", ref)
       end,
+      OnFrameCount = function() Event_FireGenericEvent("VarChange_FrameCount") end,
     },
-    
-    FourthContract = {
-      Init = function()
-        local addonContracts = Apollo.GetAddon("Contracts")
-        if not addonContracts then return end
-        local originalMethod = addonContracts.OpenContracts
-        addonContracts.OpenContracts = function(...)
-          originalMethod(...)
+    {
+      Name = "Fourth Contract",
+      Description = "stuff",
+      Load = function(ref)
+        ref.addonContracts = Apollo.GetAddon("Contracts")
+        if not ref.addonContracts then return end
+        ref.funcOriginal = ref.addonContracts.OpenContracts
+        ref.addonContracts.OpenContracts = function(...)
+          ref.funcOriginal(...)
           local arrContractWindows = {
-            addonContracts.tWndRefs.wndPvEContracts,
-            addonContracts.tWndRefs.wndPvPContracts,
+            ref.addonContracts.tWndRefs.wndPvEContracts,
+            ref.addonContracts.tWndRefs.wndPvPContracts,
           }
           for idx, wndContracts in ipairs(arrContractWindows) do
             local wndActiveContracts = wndContracts:FindChild("ActiveContracts")
@@ -39,63 +42,77 @@ function HacksByAramunn:GenHacks()
           end
         end
       end,
+      Unload = function(ref)
+        ref.addonContracts.OpenContracts = ref.funcOriginal
+      end,
     },
-    
   }
 end
 
-function HacksByAramunn:OnSlashCommand()
-  Print("Hacks by Aramunn - v@project-version@")
-  for hackName, hackData in pairs(self.tHacks) do
-    Print("  "..hackName)
-    -- Print("  "..hackName..": "..(self.tSave[hackName] and "on" or "off"))
+function HacksByAramunn:LoadMainWindow()
+  if self.wndMain and self.wndMain:IsValid() then
+    self.wndMain:Destroy()
+  end
+  self.wndMain = Apollo.LoadForm(self.xmlDoc, "Main", nil, self)
+  local wndList = self.wndMain:FindChild("List")
+  for hackName, hackData in ipairs(self.tHacks) do
+    local wndHack = Apollo.LoadForm(self.xmlDoc, "Hack", wndList, self)
+    wndHack:FindChild("IsEnabled"):SetData(hackData)
+    wndHack:FindChild("IsEnabled"):SetCheck(self.tSave[hackName] == true)
+    wndHack:FindChild("Name"):SetText(hackData.Name)
+    wndHack:FindChild("Description"):SetText(hackData.Description)
+  end
+  wndList:ArrangeChildrenVert(Window.CodeEnumArrangeOrigin.LeftOrTop)
+end
+
+function HacksByAramunn:OnEnableDisable(wndHandler, wndControl)
+  local hackData = wndControl:GetData()
+  local hackName = hackData.Name
+  self.tSave[hackName] = wndControl:IsChecked() or nil
+  if self.tSave[hackName] then
+    hackData:Load()
+  else
+    hackData:Unload()
   end
 end
 
--- function HacksByAramunn:OnSlashCommand(strCmd, strParam)
-  -- strParam = strParam and string.lower(strParam) or ""
-  -- local strOptions = ""
-  -- local nSplitIndex = string.find(strParam, " ") or 0
-  -- if nSplitIndex > 2 then
-    -- strOptions = string.sub(strParam, nSplitIndex + 1)
-    -- strParam = string.sub(strParam, 1, nSplitIndex - 1)
-  -- end
-  -- local funcCmd
-  -- for _, tCmdData in pairs(ktCommands) do
-    -- if strParam == tCmdData.strCmd then funcCmd = tCmdData.funcCmd end
-  -- end
-  -- if funcCmd then funcCmd(self, strOptions) else self:PrintHelp() end
--- end
+function HacksByAramunn:OnSave(eLevel)
+  if eLevel == GameLib.CodeEnumAddonSaveLevel.Account then
+    local tSave = {}
+    for hackName, hackStatus in pairs(self.tSave) do
+      if hackStatus then tSave[hackName] = true end
+    end
+    return tSave
+  end
+end
 
--- function HacksByAramunn:OnSave(eLevel)
-  -- if eLevel == GameLib.CodeEnumAddonSaveLevel.Account then
-    -- return self.tSave
-  -- end
--- end
-
--- function HacksByAramunn:OnRestore(eLevel, tSave)
-  -- for hackName, hackStatus in pairs(tSave) do
-    -- if self.tSave[hackName] then self.tSave[hackName] = hackStatus end
-  -- end
--- end
+function HacksByAramunn:OnRestore(eLevel, tSave)
+  for hackName, hackStatus in pairs(tSave) do
+    if hackStatus and self.tHacks[hackName] then
+      self.tSave[hackName] = true
+      self.tHacks[hackName]:Load()
+    end
+  end
+end
 
 function HacksByAramunn:new(o)
-  o = o or {}
+  o = o or { tSave = {} }
   setmetatable(o, self)
   self.__index = self
   return o
 end
 
 function HacksByAramunn:Init()
+  self:GenHacks()
   Apollo.RegisterAddon(self)
 end
 
 function HacksByAramunn:OnLoad()
-  self:GenHacks()
-  for hackName, hackData in pairs(self.tHacks) do hackData.Init() end
-  Apollo.RegisterSlashCommand("hacksbyaramunn", "OnSlashCommand", self)
-  Apollo.RegisterSlashCommand("ahacks", "OnSlashCommand", self)
-  Apollo.RegisterSlashCommand("ahax", "OnSlashCommand", self)
+  self.xmlDoc = XmlDoc.CreateFromFile("HacksByAramunn.xml")
+  self.xmlDoc:RegisterCallback("OnDocumentReady", self)
+  Apollo.RegisterSlashCommand("hacksbyaramunn", "LoadMainWindow", self)
+  Apollo.RegisterSlashCommand("ahacks", "LoadMainWindow", self)
+  Apollo.RegisterSlashCommand("ahax", "LoadMainWindow", self)
 end
 
 local HacksByAramunnInst = HacksByAramunn:new()
