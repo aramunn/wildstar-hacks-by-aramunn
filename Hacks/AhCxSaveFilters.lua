1ad29bebce255db71ead1469a17953194ddd8f14
aramunn <aramunn@users.noreply.github.com>
2017-02-27 00:17:12 -0500

    doing stuff

--- Hacks/AhCxSaveFilters.lua
+++ Hacks/AhCxSaveFilters.lua
@@ -7,73 +7,66 @@ local Hack = {
 }
 
 function Hack:Initialize()
+  local bCanUseHack = false
   self.addonMarketplaceAuction = Apollo.GetAddon("MarketplaceAuction")
-  if not self.addonMarketplaceAuction then return false end
-  local funcOriginal = self.addonMarketplaceAuction.OnItemAuctionSearchResults
-  self.addonMarketplaceAuction.OnItemAuctionSearchResults = function(...)
-    funcOriginal(...)
-    if self.bIsLoaded then
-      self:InsertGoToPage()
+  if self.addonMarketplaceAuction then
+    local funcOriginal = self.addonMarketplaceAuction.InitializeCategories
+    self.addonMarketplaceAuction.InitializeCategories = function(...)
+      if self.bIsLoaded then
+        self:InsertSaveFiltersAH()
+      end
+      funcOriginal(...)
     end
+    bCanUseHack = true
   end
-  return true
+  self.addonMarketplaceCommodity = Apollo.GetAddon("MarketplaceCommodity")
+  if self.addonMarketplaceCommodity then
+    bCanUseHack = true
+  end
+  return bCanUseHack
 end
 
 function Hack:Load()
   self.bIsLoaded = true
 end
 
-	self.wndMain:FindChild("SearchEditBox"):SetText("")
-	local strSearchQuery = tostring(self.wndMain:FindChild("SearchEditBox"):GetText())
-	self.wndMain:FindChild("SearchClearBtn"):Show(true)  Apollo.StringLength(wndHandler:GetText() or "") > 0)
-          
-	local nSearchId = self.nSearchId
-	local strSearchEnum = self.strSearchEnum
-	local arFilters = self:GetBuyFilterOptions()
-	local tSortData = self.wndMain:FindChild("SortOptionsBtn"):GetData()
-          
-          
-	local arFilters = self:GetBuyFilterOptions()
-
-	local nSearchId = self.nSearchId
-	local strSearchEnum = self.strSearchEnum
-	local strSearchQuery = tostring(self.wndMain:FindChild("SearchEditBox"):GetText())
-
-	local tSortData = self.wndMain:FindChild("SortOptionsBtn"):GetData()
-	local eAuctionSort = tSortData and tSortData.eAuctionSort or MarketplaceLib.AuctionSort.TimeLeft
-	local bReverseSort = tSortData and tSortData.bReverse or false
-	local nPropertySort = tSortData and tSortData.nPropertySort or false
-
 function Hack:InsertSaveFiltersAH()
+  if not self.addonMarketplaceAuction.wndMain then return end
+  local wndParent = self.addonMarketplaceAuction.wndMain:FindChild("MainCategoryContainer")
+  local wndTop = Apollo.LoadForm(self.addonMarketplaceAuction.xmlDoc, "CategoryTopItem", wndParent, self.addonMarketplaceAuction)
+  wndTop:FindChild("CategoryTopBtn"):SetData(wndTop)
+  wndTop:FindChild("CategoryTopBtn"):SetCheck(false) -- Check the first item found
+  wndTop:FindChild("CategoryTopBtn"):SetText("Test Top")
+  local wndCurr = Apollo.LoadForm(self.addonMarketplaceAuction.xmlDoc, "CategoryMidItem", wndTop:FindChild("CategoryTopList"), self.addonMarketplaceAuction)
+  wndCurr:FindChild("CategoryMidBtn"):SetText("Test Mid")
+  wndCurr:FindChild("CategoryMidBtn"):SetData({ 123, "Bot" })
+  -- self.nSearchId = wndHandler:GetData()[1]
+  -- self.strSearchEnum = wndHandler:GetData()[2]
 end
+
+-- self.wndMain:FindChild("SearchEditBox"):SetText("")
+-- local strSearchQuery = tostring(self.wndMain:FindChild("SearchEditBox"):GetText())
+-- self.wndMain:FindChild("SearchClearBtn"):Show(true)  Apollo.StringLength(wndHandler:GetText() or "") > 0)
+        
+-- local nSearchId = self.nSearchId
+-- local strSearchEnum = self.strSearchEnum
+-- local arFilters = self:GetBuyFilterOptions()
+-- local tSortData = self.wndMain:FindChild("SortOptionsBtn"):GetData()
+        
+        
+-- local arFilters = self:GetBuyFilterOptions()
+
+-- local nSearchId = self.nSearchId
+-- local strSearchEnum = self.strSearchEnum
+-- local strSearchQuery = tostring(self.wndMain:FindChild("SearchEditBox"):GetText())
+
+-- local tSortData = self.wndMain:FindChild("SortOptionsBtn"):GetData()
+-- local eAuctionSort = tSortData and tSortData.eAuctionSort or MarketplaceLib.AuctionSort.TimeLeft
+-- local bReverseSort = tSortData and tSortData.bReverse or false
+-- local nPropertySort = tSortData and tSortData.nPropertySort or false
 
 function Hack:InsertSaveFiltersCX()
 end
-
--- function Hack:InsertGoToPage()
-  if not self.addonMarketplaceAuction.wndMain then return end
-  local wndToModify = self.addonMarketplaceAuction.wndMain
-  local arrWindows = {
-    "BuyContainer",
-    "SearchResultList",
-    "BuyPageBtnContainer",
-  }
-  for idx, strWindow in ipairs(arrWindows) do
-    wndToModify = wndToModify:FindChild(strWindow)
-    if not wndToModify then return end
-  end
-  local wndGoToPage = Apollo.LoadForm(self.xmlDoc, "GoToPage", wndToModify, self)
-  wndGoToPage:FindChild("PageNumber"):SetText(tostring(self.addonMarketplaceAuction.nCurPage + 1))
--- end
-
--- function Hack:OnGoTo(wndHandler, wndControl)
-  local nMaxPage = math.floor(self.addonMarketplaceAuction.nTotalResults / MarketplaceLib.kAuctionSearchPageSize)
-  local wndPage = wndControl:GetParent():FindChild("PageNumber")
-  local nPage = tonumber(wndPage:GetText()) - 1
-  if nPage < 0 then nPage = 0 end
-  if nPage > nMaxPage then nPage = nMaxPage end
-  self.addonMarketplaceAuction.fnLastSearch(nPage)
--- end
 
 function Hack:Unload()
   self.bIsLoaded = false
