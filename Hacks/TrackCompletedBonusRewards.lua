552221c0ba02039b0a4f571e2b379578ece4f9e0
aramunn <aramunn@users.noreply.github.com>
2017-03-10 01:02:15 -0500

    manual marking works?

--- Hacks/TrackCompletedBonusRewards.lua
+++ Hacks/TrackCompletedBonusRewards.lua
@@ -13,7 +13,7 @@ function Hack:Initialize()
   self.addonMatchMaker.BuildFeaturedList = function(...)
     funcOriginal(...)
     if self.bIsLoaded then
-      self:PlaceOverlay()
+      self:PlaceOverlays()
     end
   end
   return true
@@ -24,18 +24,68 @@ function Hack:Load()
   -- Apollo.RegisterEventHandler("ChannelUpdate_Loot", "OnChannelUpdate_Loot", self)
 end
 
-function Hack:PlaceOverlay()
+function Hack:PlaceOverlays()
+  local tCurrentTime = GameLib.GetServerTime()
+  local nCurrentSeconds = os.time({
+    ["year"]  = tCurrentTime.nYear,
+    ["month"] = tCurrentTime.nMonth,
+    ["day"]   = tCurrentTime.nDay,
+    ["hour"]  = tCurrentTime.nHour,
+    ["min"]   = tCurrentTime.nMinute,
+    ["sec"]   = tCurrentTime.nSecond,
+  })
   local wndList = self.addonMatchMaker.tWndRefs.wndMain:FindChild("TabContent:RewardContent")
   for idx, wndReward in ipairs(wndList:GetChildren()) do
-    local tRewardListEntry = wndReward:FindChild("InfoButton"):GetData()
-    local wndOverlay = Apollo.LoadForm(self.xmlDoc, "Overlay", wndReward, self)
-    if idx%2 == 0 then wndOverlay:FindChild("Shader"):Show(false) end
-    -- Print(tostring(tRewardListEntry.strContentName))
+    self:BuildOverlay(wndReward, nCurrentSeconds)
   end
 end
 
-function Hack:OnCompleted(wndHandler, wndControl)
-  Print("here")
+function Hack:BuildOverlay(wndReward, nCurrentSeconds)
+  local tRewardListEntry = wndReward:FindChild("InfoButton"):GetData()
+  local tReward = {
+    strContentName = tRewardListEntry.strContentName,
+    nEndTime = nCurrentSeconds + tRewardListEntry.nSecondsRemaining,
+    nMultiplier = tRewardListEntry.tRewardInfo.nMultiplier,
+  }
+  local wndOverlay = Apollo.LoadForm(self.xmlDoc, "Overlay", wndReward, self)
+  wndOverlay:FindChild("Completed"):SetData(tReward)
+  if self:RewardActive(tReward) then
+    wndOverlay:FindChild("Shader"):Show(false)
+  else
+    wndOverlay:FindChild("Completed"):SetCheck(true)
+  end
+end
+
+function Hack:RewardActive(tReward)
+  for idx, tCompletedReward in ipairs(self.tSave) do
+    if self:IsSameReward(tReward, tCompletedReward) then return false end
+  end
+  return true
+end
+
+function Hack:IsSameReward(tA, tB)
+  if tA.nMultiplier ~= tB.nMultiplier then return false end
+  local nEndDiff = math.abs(tA.nEndTime - tB.nEndTime)
+  if nEndDiff > 60 then return false end
+  if tA.strContentName ~= tB.strContentName then return false end
+  return true
+end
+
+function Hack:OnCompletedCheck(wndHandler, wndControl)
+  table.insert(self.tSave, wndControl:GetData())
+  wndControl:GetParent():FindChild("Shader"):Show(true)
+end
+
+function Hack:OnCompletedUncheck(wndHandler, wndControl)
+  local tReward = wndControl:GetData()
+  local tTmpSave = self.tSave
+  self.tSave = {}
+  for idx, tCompletedReward in ipairs(tTmpSave) do
+    if not self:IsSameReward(tReward, tCompletedReward) then
+      table.insert(self.tSave, tCompletedReward)
+    end
+  end
+  wndControl:GetParent():FindChild("Shader"):Show(false)
 end
 
 function Hack:OnChannelUpdate_Loot(eType, tEventArgs)
@@ -52,13 +102,14 @@ function Hack:OnChannelUpdate_Loot(eType, tEventArgs)
       strType = eType == Money.CodeEnumCurrencyType.BlueEssence and "Blue" or strType
       strType = eType == Money.CodeEnumCurrencyType.GreenEssence and "Green" or strType
       strType = eType == Money.CodeEnumCurrencyType.PurpleEssence and "Purple" or strType
-      Print(strType..": "..tostring(tEventArgs.monNew:GetAmount()).." (+"..tostring(tEventArgs.monSignatureBonus:GetAmount())..") [x"..tostring(tEventArgs.monEssenceBonus:GetAmount()).."]")
+      -- Print(strType..": "..tostring(tEventArgs.monNew:GetAmount()).." (+"..tostring(tEventArgs.monSignatureBonus:GetAmount())..") [x"..tostring(tEventArgs.monEssenceBonus:GetAmount()).."]")
     end
   end
 end
 
 function Hack:Unload()
   self.bIsLoaded = false
+  -- Apollo.RemoveEventHandler("ChannelUpdate_Loot", self)
 end
 
 function Hack:new(o)
